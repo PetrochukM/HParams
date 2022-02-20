3d0b7fa3604f85e6505400c11957a0dbc992d715
deepblue129 <petrochukm@gmail.com>
2022-02-20 09:07:48 -0800

    Delete unused files

--- _tests/other_module.py
+++
@@ -1,9 +0,0 @@
-""" A module outside of `tests` used for testing `hparams`. """
-
-from hparams import configurable
-from hparams import HParam
-
-
-@configurable
-def configured(arg=HParam()):
-    return arg
